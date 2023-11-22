31db217d37f1879e3496ce535c4e7355f0cc759f
Giuseppe Maffia <gmaffia596@gmail.com>
2023-11-22 10:24:35 +0100

    add dummy controller

---
+++ DevWorld.LaContessa/DevWorld.LaContessa.API/Controllers/DummyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevWorld.LaContessa.API.Controllers;
+
+[ApiController]
+[Route("dummy")]
+public class DummyController : ControllerBase
+{
+    public DummyController() { }
+
+    [HttpGet]
+    public async Task<ActionResult<string>> GetDummy(CancellationToken cancellationToken)
+    {
+        return "Good job dummy";
+    }
+}
