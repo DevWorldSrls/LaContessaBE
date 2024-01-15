b18ab0e5b8920b2a4691bbb0311d2601e3452608
Giuseppe Maffia <gmaffia596@gmail.com>
2024-01-15 11:09:33 +0100

    remove password from user models

--- DevWorld.LaContessa/DevWorld.LaContessa.Query.Abstractions/Users/GetUser.cs
+++ DevWorld.LaContessa/DevWorld.LaContessa.Query.Abstractions/Users/GetUser.cs
@@ -23,7 +23,6 @@ public class GetUser : IRequest<GetUser.Response>
             public string Name { get; set; } = null!;
             public string Surname { get; set; } = null!;
             public string Email { get; set; } = null!;
-            public string Password { get; set; } = null!;
             public bool IsPro { get; set; } = false;
             public bool PeriodicBookingsEnabled { get; set; } = false;
             public string? ImageProfile { get; set; }
--- DevWorld.LaContessa/DevWorld.LaContessa.Query.Abstractions/Users/GetUsers.cs
+++ DevWorld.LaContessa/DevWorld.LaContessa.Query.Abstractions/Users/GetUsers.cs
@@ -14,7 +14,6 @@ public class GetUsers : IRequest<GetUsers.Response>
             public string Name { get; set; } = null!;
             public string Surname { get; set; } = null!;
             public string Email { get; set; } = null!;
-            public string Password { get; set; } = null!;
             public bool IsPro { get; set; } = false;
             public bool PeriodicBookingsEnabled { get; set; } = false;
             public string? ImageProfile { get; set; }
